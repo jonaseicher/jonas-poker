f00cbfe163ca8a5f406619596a7148ab32e37762
Jonas Eicher <jonas.maximilian.eicher@gmail.com>
2020-05-31 22:10:47 +0200

    v-menu, logout

--- frontend/src/views/AccountComponent.vue
+++ frontend/src/views/AccountComponent.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    <v-btn v-if="auth.isAuthenticated" @click="logout">
-      <v-img v-if="auth.user.picture" :src="auth.user.picture" max-width="30px"></v-img>
-      <v-icon v-else>mdi-account</v-icon>
-      <v-spacer class="px-2"></v-spacer>
-      {{ auth.user.name }}
-    </v-btn>
+    <v-menu v-if="auth.isAuthenticated" offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on">
+          <v-img v-if="auth.user.picture" :src="auth.user.picture" max-width="30px"></v-img>
+          <v-icon v-else>mdi-account</v-icon>
+          <v-spacer class="px-2"></v-spacer>
+          {{ auth.user.name }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item @click="logout">
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <v-btn v-else @click="login">
+       <v-icon class="pr-2">mdi-login</v-icon>
       Login
     </v-btn>
   </div>
