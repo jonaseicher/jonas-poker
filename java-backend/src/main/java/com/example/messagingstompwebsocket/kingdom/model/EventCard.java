2a6913a6e4ed58558f310324042afdaf91fb4693
noharms <47367811+noharms@users.noreply.github.com>
2020-06-25 21:22:35 +0200

    wip .. decision on eventCard

--- java-backend/src/main/java/com/example/messagingstompwebsocket/kingdom/model/EventCard.java
+++ java-backend/src/main/java/com/example/messagingstompwebsocket/kingdom/model/EventCard.java
@@ -4,5 +4,8 @@ import lombok.Data;
 
 @Data
 public class EventCard implements Card {
+    int ID;
     String name;
+    String description;
+    Map<String, String> player2decision;
 }
--- java-backend/src/main/java/com/example/messagingstompwebsocket/kingdom/model/Townsman.java
+++ java-backend/src/main/java/com/example/messagingstompwebsocket/kingdom/model/Townsman.java
@@ -1,9 +1,12 @@
 package com.example.messagingstompwebsocket.kingdom.model;
 
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
 public class Townsman {
+  @Id
   String name;
   int wealth;
 }
